71cf9169876a99b41f3168ce2c8aefde5401007f
Tom Zielinski <tomzielinski@live.ca>
2021-03-31 09:46:14 -0400

    added mongoose

---
+++ Config/db.js
@@ -0,0 +1,8 @@
+"use strict";
+let mondoDBPath = "mongodb://localhost/webd6201";
+let sessionSecret = "someSecret";
+module.exports = {
+    Path: mondoDBPath,
+    Secret: sessionSecret,
+};
+//# sourceMappingURL=db.js.map
---
+++ Config/db.ts
@@ -0,0 +1,7 @@
+let mondoDBPath = "mongodb://localhost/webd6201";
+let sessionSecret = "someSecret";
+
+module.exports = {
+  Path: mondoDBPath,
+  Secret: sessionSecret,
+};
